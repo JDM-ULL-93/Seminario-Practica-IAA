01a1e03f6e8212bc7222e52dc752b6b0dc1c5e3e
alu0101160337 <alu0101160337@ull.edu.es>
2020-03-29 14:48:38 +0100

    Proyecto listo para empezar a ser codificado con API Netica

--- src/compile.sh
+++ src/compile.sh
@@ -3,3 +3,6 @@ echo "compiling"
 
 gcc NeticaEx.c -c -I. -L../bin 
 cp -f NeticaEx.o ../lib
+rm ./NeticaEx.o
+
+gcc ../lib/NeticaEx.o Main.cpp -o Main.a -I. -L../lib/64bit -lrt -lnetica -lpthread -lstdc++ -lm
